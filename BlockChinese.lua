d1695ecb2dcf41e31ac2f029a9a0cfbad2ec4ab6
Ketho <ngsc1@live.cn>
2011-11-30 21:31:37 +0100

    Updated for 4.3

--- BlockChinese.lua
+++ BlockChinese.lua
@@ -2,7 +2,7 @@
 --- Author: Ketho (EU-Boulderfist)		---
 --- License: Public Domain				---
 --- Created: 2011.11.09					---
---- Version: v0.2 [2011.11.09]			---
+--- Version: v0.3 [2011.11.30]			---
 -------------------------------------------
 --- Curse			http://www.curse.com/addons/wow/blockchinese
 --- WoWInterface	http://www.wowinterface.com/downloads/info20488-BlockChinese.html
@@ -15,6 +15,11 @@ local events = {
 	"CHAT_MSG_SAY",
 	"CHAT_MSG_YELL",
 	"CHAT_MSG_CHANNEL",
+	--"CHAT_MSG_EMOTE",
+	--"CHAT_MSG_WHISPER",
+	--"CHAT_MSG_PARTY",
+	--"CHAT_MSG_RAID",
+	--"CHAT_MSG_BATTLEGROUND",
 }
 
 local function filter(self, event, msg, ...)
