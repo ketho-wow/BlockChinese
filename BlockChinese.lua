224491fd41e292b878762df2de02d61bff482e47
Ketho <ngsc1@live.cn>
2014-10-17 00:49:01 +0200

    Update for WoD

--- BlockChinese.lua
+++ BlockChinese.lua
@@ -2,7 +2,7 @@
 --- Author: Ketho (EU-Boulderfist)		---
 --- License: Public Domain				---
 --- Created: 2011.11.09					---
---- Version: 0.8 [2013.12.15]			---
+--- Version: 0.9 [2014.10.17]			---
 -------------------------------------------
 --- Curse			http://www.curse.com/addons/wow/blockchinese
 --- WoWInterface	http://www.wowinterface.com/downloads/info20488-BlockChinese.html
@@ -15,11 +15,15 @@ local events = {
 	"CHAT_MSG_SAY",
 	"CHAT_MSG_YELL",
 	"CHAT_MSG_CHANNEL",
-	"CHAT_MSG_WHISPER",
-	--"CHAT_MSG_EMOTE",
+	"CHAT_MSG_TEXT_EMOTE",
+	--"CHAT_MSG_WHISPER", -- if its intended for you then maybe not block it
+	--"CHAT_MSG_GUILD",
 	--"CHAT_MSG_PARTY",
+	--"CHAT_MSG_PARTY_LEADER",
 	--"CHAT_MSG_RAID",
+	--"CHAT_MSG_RAID_LEADER",
 	--"CHAT_MSG_INSTANCE_CHAT",
+	--"CHAT_MSG_INSTANCE_CHAT_LEADER",
 }
 
 local function filter(self, event, msg, ...)
@@ -28,6 +32,6 @@ local function filter(self, event, msg, ...)
 	end
 end
 
-for _, v in ipairs(events) do
+for _, v in pairs(events) do
 	ChatFrame_AddMessageEventFilter(v, filter)
 end
