61344bc829b60fd9c6cb8b00d883005f80532d8a
Ketho <ngsc1@live.cn>
2014-11-07 16:56:29 +0800

    Include Japanese and Korean again to stop font size abuse

--- BlockChinese.lua
+++ BlockChinese.lua
@@ -2,7 +2,7 @@
 --- Author: Ketho (EU-Boulderfist)		---
 --- License: Public Domain				---
 --- Created: 2011.11.09					---
---- Version: 0.9 [2014.10.17]			---
+--- Version: 1.0 [2014.11.07]			---
 -------------------------------------------
 --- Curse			http://www.curse.com/addons/wow/blockchinese
 --- WoWInterface	http://www.wowinterface.com/downloads/info20488-BlockChinese.html
@@ -16,7 +16,7 @@ local events = {
 	"CHAT_MSG_YELL",
 	"CHAT_MSG_CHANNEL",
 	"CHAT_MSG_TEXT_EMOTE",
-	--"CHAT_MSG_WHISPER", -- if its intended for you then maybe not block it
+	"CHAT_MSG_WHISPER",
 	--"CHAT_MSG_GUILD",
 	--"CHAT_MSG_PARTY",
 	--"CHAT_MSG_PARTY_LEADER",
@@ -27,7 +27,7 @@ local events = {
 }
 
 local function filter(self, event, msg, ...)
-	if strfind(msg, "[\228-\233]") then
+	if strfind(msg, "[\227-\237]") then
 		return true
 	end
 end
